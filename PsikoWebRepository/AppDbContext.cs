ca968c7a2bb4a8cde7145e479bca62053270d841
Thenough <m.cannn4141@gmail.com>
2024-08-11 15:47:15 +0300

    CreateDate and UpdateDate Added

--- PsikoWebRepository/AppDbContext.cs
+++ PsikoWebRepository/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Core.Models.Concrete;
+﻿using Core.Models.Abstarct;
+using Core.Models.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -16,11 +17,66 @@ namespace Repository
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Category> Category { get; set; }
         public DbSet<Product> Product { get; set; }
+
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity baseEntity)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                baseEntity.CreateDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(baseEntity).Property(x => x.CreateDate).IsModified = false;
+                                baseEntity.UpdateDate = DateTime.Now;
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if(item.Entity is BaseEntity baseEntity)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                baseEntity.CreateDate = DateTime.Now;
+                                break;
+                            }
+                            case EntityState.Modified:
+                            {
+                                Entry(baseEntity).Property(x => x.CreateDate).IsModified = false;
+                                baseEntity.UpdateDate = DateTime.Now;
+                                break;
+                            }
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(modelBuilder);
         }
+
     }
 }
